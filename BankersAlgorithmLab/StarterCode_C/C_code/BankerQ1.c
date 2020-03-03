d94d40552d51358f0e8d854c69b978f32e07f55e
Low Entropy Body <45514917+sutdorg@users.noreply.github.com>
2020-03-03 12:35:43 +0800

    half done

--- BankersAlgorithmLab/StarterCode_C/C_code/BankerQ1.c
+++ BankersAlgorithmLab/StarterCode_C/C_code/BankerQ1.c
@@ -10,38 +10,46 @@
 int numberOfCustomers = 0; // the number of customers
 int numberOfResources = 0; // the number of resources
 
-int *available;       // the available amount of each resource
-int **maximum;        // the maximum demand of each customer
-int **allocation;     // the amount currently allocated
-int **need;           // the remaining needs of each customer
+int *available;   // the available amount of each resource
+int **maximum;	// the maximum demand of each customer
+int **allocation; // the amount currently allocated
+int **need;		  // the remaining needs of each customer
 
 // Utility function to allocate an int vector.
-int *mallocIntVector(int size) {
+int *mallocIntVector(int size)
+{
 	int i, *a = malloc(sizeof(int) * size);
-	for (i = 0; i < size; i++) a[i] = 0;
+	for (i = 0; i < size; i++)
+		a[i] = 0;
 	return a;
 }
 
 // Utility function to free an int vector.
-void freeIntVector(int *a) {
+void freeIntVector(int *a)
+{
 	free(a);
 }
 
 // Utility function to allocate an int matrix.
-int **mallocIntMatrix(int numRows, int numColumns) {
-	int i, j, **a = malloc(sizeof(int*) * (numRows+1));
-	for (i = 0; i < numRows; i++) {
+int **mallocIntMatrix(int numRows, int numColumns)
+{
+	int i, j, **a = malloc(sizeof(int *) * (numRows + 1));
+	for (i = 0; i < numRows; i++)
+	{
 		a[i] = malloc(sizeof(int) * numColumns);
-		for (j = 0; j < numColumns; j++) a[i][j] = 0;
+		for (j = 0; j < numColumns; j++)
+			a[i][j] = 0;
 	}
 	a[numRows] = 0;
 	return a;
 }
 
 // Utility function to free an int matrix.
-void freeIntMatrix(int **a) {
+void freeIntMatrix(int **a)
+{
 	int i;
-	for (i = 0; a[i] != 0; i++) free(a[i]);
+	for (i = 0; a[i] != 0; i++)
+		free(a[i]);
 	free(a);
 }
 
@@ -51,23 +59,27 @@ void freeIntMatrix(int **a) {
  * @param m          The number of resources.
  * @param n          The number of customers.
  */
-void initBank(int *resources, int m, int n) {
+void initBank(int *resources, int m, int n)
+{
 	// Allocate memory for the vector and matrices
 	available = mallocIntVector(m);
 	need = mallocIntMatrix(n, m);
 	allocation = mallocIntMatrix(n, m);
 	maximum = mallocIntMatrix(n, m);
-	
-	// TODO: initialize the numberOfCustomers and numberOfResources
-	
-	// TODO: initialize the available vector
 
+	// TODO: initialize the numberOfCustomers and numberOfResources
+	numberOfCustomers = n;
+	numberOfResources = m;
+
+	// TODO: initialize the available vector
+	available = resources;
 }
 
 /**
  * Frees the memory used to store the state of the bank.
  */
-void freeBank() {
+void freeBank()
+{
 	freeIntVector(available);
 	freeIntMatrix(need);
 	freeIntMatrix(allocation);
@@ -77,7 +89,8 @@ void freeBank() {
 /**
  * Prints the state of the bank.
  */
-void printState() {
+void printState()
+{
 	int i;
 	int j;
 	// TODO: print the current state with a tidy format
@@ -85,41 +98,47 @@ void printState() {
 
 	// TODO: print available
 	printf("Available:\n");
-	for (i=0; i<numberOfResources; i++){
-		printf("%d ",available[i]);  // leave a space between each print 
+	for (i = 0; i < numberOfResources; i++)
+	{
+		printf("%d ", available[i]); // leave a space between each print
 	}
-	printf("\n"); // Leave a line after printing available 
+	printf("\n"); // Leave a line after printing available
 
 	// TODO: print maximum
 	printf("Maximum:\n");
-	for (i=0; i<numberOfCustomers; i++){
-		for (j=0; j<numberOfResources; j++){
-			printf("%d ", maximum[i][j]); // Leave a space between each print 
+	for (i = 0; i < numberOfCustomers; i++)
+	{
+		for (j = 0; j < numberOfResources; j++)
+		{
+			printf("%d ", maximum[i][j]); // Leave a space between each print
 		}
-		printf("\n"); // Leave a line after each customer 
+		printf("\n"); // Leave a line after each customer
 	}
 	printf("\n"); // Leave a line after printing maximum
 
 	// TODO: print allocation
 	printf("Allocation:\n");
-	for (i=0; i<numberOfCustomers; i++){
-		for (j=0; j<numberOfResources; j++){
-			printf("%d ", allocation[i][j]); // Leave a space between each print 
+	for (i = 0; i < numberOfCustomers; i++)
+	{
+		for (j = 0; j < numberOfResources; j++)
+		{
+			printf("%d ", allocation[i][j]); // Leave a space between each print
 		}
-		printf("\n"); // Leave a line after each customer 
-	} 
+		printf("\n"); // Leave a line after each customer
+	}
 	printf("\n"); // Leave a line after printing Allcoation
 
 	// TODO: print need
 	printf("Need:\n");
-	for (i=0; i<numberOfCustomers; i++){
-		for (j=0; j<numberOfResources; j++){
-			printf("%d ", need[i][j]); // Leave a space bettern each print 
+	for (i = 0; i < numberOfCustomers; i++)
+	{
+		for (j = 0; j < numberOfResources; j++)
+		{
+			printf("%d ", need[i][j]); // Leave a space bettern each print
 		}
-		printf("\n"); // Leave a line after each customer 
-	} 
+		printf("\n"); // Leave a line after each customer
+	}
 	printf("\n"); // Leave a line after printing need
-
 }
 
 /**
@@ -127,9 +146,13 @@ void printState() {
  * @param customerIndex  The customer's index (0-indexed).
  * @param maximumDemand  An array of the maximum demanded count for each resource.
  */
-void setMaximumDemand(int customerIndex, int *maximumDemand) {
+void setMaximumDemand(int customerIndex, int *maximumDemand)
+{
 	// TODO: add customer, update maximum and need
-
+	for (int i = 0; i < numberOfResources; i++)
+	{
+		maximum[customerIndex][i] = maximumDemand[i];
+	}
 }
 
 /**
@@ -139,19 +162,82 @@ void setMaximumDemand(int customerIndex, int *maximumDemand) {
  * @return 1 if the requested resources will leave the bank in a
  *         safe state, else 0
  */
-int checkSafe(int customerIndex, int *request) {
+int checkSafe(int customerIndex, int *request)
+{
 	// Allocate temporary memory to copy the bank state.
 	int *work = mallocIntVector(numberOfResources);
 	int **tempNeed = mallocIntMatrix(numberOfCustomers, numberOfResources);
 	int **tempAllocation = mallocIntMatrix(numberOfCustomers, numberOfResources);
-	
+
 	// TODO: copy the bank's state to the temporary memory and update it with the request.
-	
+	for (int i = 0; i < numberOfResources; i++)
+	{
+		// work = available - request;
+		// printf("%d \n", request);
+		work[i] = available[i] - request[i];
+		// printf("test\n");
+	}
+
+	for (int i = 0; i < numberOfResources; i++)
+	{
+		// tempNeed = need - request;
+		tempNeed[customerIndex][i] = need[customerIndex][i] - request[i];
+	}
+	for (int i = 0; i < numberOfResources; i++)
+	{
+		// tempAllocation = allocation;
+		tempAllocation[customerIndex][i] = allocation[customerIndex][i] + request[i];
+	}
 	// TODO: check if the new state is safe
+	int *customerFinish = mallocIntVector(numberOfCustomers);
+	for (int i = 0; i < numberOfCustomers; i++)
+	{
+		customerFinish[i] = 0;
+	}
+	int possible = 1;
+	while (possible == 1)
+	{
+		possible = 0;
+		for (int customerIndex = 0; customerIndex < numberOfCustomers; customerIndex++)
+		{
+			if (customerFinish[customerIndex] == 0)
+			{
+				int checker = 1;
+				for (int i = 0; i < numberOfResources; i++)
+				{
+					if (tempNeed[customerIndex][i] > work[i])
+					{
+						checker = 0;
+					}
+				}
+				if (checker == 1)
+				{
+					possible = 1;
+					for (int i = 0; i < numberOfResources; i++)
+					{
+						work[i] = work[i] + tempAllocation[customerIndex][i];
+					}
+					customerFinish[customerIndex] = 1;
+				}
+			}
+		}
+	}
+	// freeIntVector(work);
+	// freeIntMatrix(tempNeed);
+	// freeIntMatrix(tempAllocation);
+
+	for (int i = 0; i < numberOfCustomers; i++)
+	{
+		if (customerFinish[i] == 0)
+		{
+			// freeIntVector(customerFinish);
+			return 0;
+		}
+	}
+	// freeIntVector(customerFinish);
 
 	return 1;
 }
-
 /**
  * Requests resources for a customer loan.
  * If the request leave the bank in a safe state, it is carried out.
@@ -159,18 +245,60 @@ int checkSafe(int customerIndex, int *request) {
  * @param request        An array of the requested count for each resource.
  * @return 1 if the requested resources can be loaned, else 0.
  */
-int requestResources(int customerIndex, int *request) {
+int requestResources(int customerIndex, int *request)
+{
 	// TODO: print the request
 	printf("Customer %d requesting\n", customerIndex);
-	
+	for (int i = 0; i < numberOfResources; i++)
+	{
+		printf("%d ", request[i]);
+	}
+	printf("\n");
+
 	// TODO: judge if request larger than need
-	
+	for (int i = 0; i < numberOfResources; i++)
+	{
+		if (request[i] > need[customerIndex][i])
+		{
+			return 0;
+		}
+	}
 	// TODO: judge if request larger than available
-	
+	for (int i = 0; i < numberOfResources; i++)
+	{
+		if (request[i] > available[i])
+		{
+			return 0;
+		}
+	}
+
 	// TODO: judge if the new state is safe if grants this request (for question 2)
-	
+	int safeOrNot = checkSafe(customerIndex, request);
+
 	// TODO: request is granted, update state
+	if (safeOrNot == 1)
+	{
+		for (int i = 0; i < numberOfResources; i++)
+		{
+			// work = available - request;
+			available[i] = available[i] - request[i];
+		}
+		for (int i = 0; i < numberOfResources; i++)
+		{
+			// tempNeed = need - request;
+			need[customerIndex][i] = need[customerIndex][i] - request[i];
+		}
+		for (int i = 0; i < numberOfResources; i++)
+		{
+			// tempAllocation = allocation;
+			allocation[customerIndex][i] = allocation[customerIndex][i] + request[i];
+		}
+	} else
+	{
+		return 0;
+	}
 	
+
 	return 1;
 }
 
@@ -179,11 +307,11 @@ int requestResources(int customerIndex, int *request) {
  * @param customerIndex  The customer's index (0-indexed).
  * @param release        An array of the release count for each resource.
  */
-void releaseResources(int customerIndex, int *release) {
+void releaseResources(int customerIndex, int *release)
+{
 	// TODO: print the release
 	printf("Customer %d releasing\n", customerIndex);
 	// TODO: deal with release (:For simplicity, we do not judge the release request, just update directly)
-	
 }
 
 /**
@@ -191,15 +319,19 @@ void releaseResources(int customerIndex, int *release) {
  * Provided for your convenience.
  * @param filename  The name of the file.
  */
-void runFile(const char * filename) {
+void runFile(const char *filename)
+{
 	FILE *fp = fopen(filename, "r");
 	int c = 0, i = 0, j = 0, m = 0, n = 0, bankInited = 0,
-	lineLen = 0, prevLineEnd = 0, maxLineLen = 0;
-	do {
-		if (c == '\n' || c == EOF) {
+		lineLen = 0, prevLineEnd = 0, maxLineLen = 0;
+	do
+	{
+		if (c == '\n' || c == EOF)
+		{
 			lineLen = i - prevLineEnd;
 			prevLineEnd = i;
-			if (lineLen > maxLineLen) maxLineLen = lineLen;
+			if (lineLen > maxLineLen)
+				maxLineLen = lineLen;
 		}
 		i++;
 	} while ((c = fgetc(fp)) != EOF);
@@ -208,64 +340,86 @@ void runFile(const char * filename) {
 	lineLen++;
 	char *line = malloc(lineLen), *token;
 	i = 0;
-	while (fgets(line, lineLen, fp) != NULL) {
-		for (j = 0; j < lineLen-1; j++) 
-			if (line[j] == '\n') line[j] = '\0';
-		if (i == 0) {
+	while (fgets(line, lineLen, fp) != NULL)
+	{
+		for (j = 0; j < lineLen - 1; j++)
+			if (line[j] == '\n')
+				line[j] = '\0';
+		if (i == 0)
+		{
 			token = strtok(line, ",");
 			token = strtok(NULL, ",");
 			n = atoi(token);
-		} else if (i == 1) {
+		}
+		else if (i == 1)
+		{
 			token = strtok(line, ",");
 			token = strtok(NULL, ",");
 			m = atoi(token);
-		} else if (i == 2) {
+		}
+		else if (i == 2)
+		{
 			token = strtok(line, ",");
 			token = strtok(NULL, ",");
 			int *resources = malloc(sizeof(int) * m);
-			for (j = 0; j < m; j++) {
+			for (j = 0; j < m; j++)
+			{
 				resources[j] = atoi(strtok(j == 0 ? token : NULL, " "));
 			}
 			initBank(resources, m, n);
 			bankInited = 1;
 			free(resources);
-		} else {
+		}
+		else
+		{
 			int *resources = malloc(sizeof(int) * m);
 			token = strtok(line, ",");
-			if (strcmp(token, "c") == 0) {
-				int customerIndex = atoi(strtok(NULL, ","));	
+			if (strcmp(token, "c") == 0)
+			{
+				int customerIndex = atoi(strtok(NULL, ","));
 				int *resources = malloc(sizeof(int) * m);
 				token = strtok(NULL, ",");
-				for (j = 0; j < m; j++) {
+				for (j = 0; j < m; j++)
+				{
 					resources[j] = atoi(strtok(j == 0 ? token : NULL, " "));
 				}
 				setMaximumDemand(customerIndex, resources);
 				free(resources);
-			} else if (strcmp(token, "r") == 0) {
-				int customerIndex = atoi(strtok(NULL, ","));	
+			}
+			else if (strcmp(token, "r") == 0)
+			{
+				int customerIndex = atoi(strtok(NULL, ","));
 				int *resources = malloc(sizeof(int) * m);
 				token = strtok(NULL, ",");
-				for (j = 0; j < m; j++) {
+				for (j = 0; j < m; j++)
+				{
 					resources[j] = atoi(strtok(j == 0 ? token : NULL, " "));
 				}
 				requestResources(customerIndex, resources);
 				free(resources);
-			} else if (strcmp(token, "f") == 0) {
-				int customerIndex = atoi(strtok(NULL, ","));	
+			}
+			else if (strcmp(token, "f") == 0)
+			{
+				int customerIndex = atoi(strtok(NULL, ","));
 				int *resources = malloc(sizeof(int) * m);
 				token = strtok(NULL, ",");
-				for (j = 0; j < m; j++) {
+				for (j = 0; j < m; j++)
+				{
 					resources[j] = atoi(strtok(j == 0 ? token : NULL, " "));
 				}
 				releaseResources(customerIndex, resources);
 				free(resources);
-			} else if (strcmp(token, "p") == 0) {
+			}
+			else if (strcmp(token, "p") == 0)
+			{
+				// printf("test\n");
 				printState();
 			}
 		}
 		i++;
 	}
-	if (bankInited) freeBank();
+	if (bankInited)
+		freeBank();
 	free(line);
 	fclose(fp);
 }
@@ -274,13 +428,11 @@ void runFile(const char * filename) {
  * Main function
  * @param args  The command line arguments
  */
-int main (int argc, const char ** argv) 
-{	
-	if (argc > 1) {
-		runFile(argv[1]);	
+int main(int argc, const char **argv)
+{
+	if (argc > 1)
+	{
+		runFile(argv[1]);
 	}
 	return 0;
 }
-
-
-
